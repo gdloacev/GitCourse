064a91018a2cef8a4a9e5296632adb7f127a1582
Oscar Aceves <itan040@hotmail.com>
2025-04-25 21:14:27 -0600

    Player Movement Script Added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f; // Speed of the player movement
+
+    private Rigidbody2D rb; // Reference to the Rigidbody2D component
+    private Vector2 movement; // Store the movement input
+
+
+    private void Awake() => rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component when the script is loaded
+
+    private void Update()
+    {
+        var moveX = Input.GetAxisRaw("Horizontal"); // Get horizontal input (A/D or Left/Right arrow keys)
+        var moveY = Input.GetAxisRaw("Vertical"); // Get vertical input (W/S or Up/Down arrow keys)
+
+        movement = new Vector2(moveX, moveY).normalized; // Create a normalized vector for movement
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); // Move the player using Rigidbody2D
+    }
+}
